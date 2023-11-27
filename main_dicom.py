0effb411a9113d3f93ef4a4bd5bad3a1d944000e
rubenexpensive <107783098+rubenexpensive@users.noreply.github.com>
2023-11-27 15:25:12 -0500

    mysql main_dicom.py

--- main_dicom.py
+++ main_dicom.py
@@ -1,23 +1,25 @@
 import sys
 from PyQt5.QtWidgets import QApplication, QMainWindow
 from vista_dicom import Vista
-from modelo_dicom import basededatos
+from modelo_dicom import *
 from controlador_dicom import Coordinador
+
 def main_dicom():
 
     mi_vista = Vista()
     mi_modelo = basededatos()
-    mi_coordinador = Coordinador(mi_vista, mi_modelo)
+    mi_db_modelo = MySQL()
+    mi_coordinador = Coordinador(mi_vista, mi_modelo, mi_db_modelo)
     mi_vista.setCoordinador(mi_coordinador)
     mi_vista.setup()
     return mi_vista
 
-if __name__ == "__main__":
-    app=QApplication(sys.argv)
-    main_window=QMainWindow()
-    main_dicom_window=main_dicom()
-    main_window.setCentralWidwet(main_dicom_window)
-    main_window.show()
-    sys.exit(app.exec_())
+# if __name__ == "__main__":
+#     app=QApplication(sys.argv)
+#     main_window=QMainWindow()
+#     main_dicom_window=main_dicom()
+#     main_window.setCentralWidwet(main_dicom_window)
+#     main_window.show()
+#     sys.exit(app.exec_())
 
 
