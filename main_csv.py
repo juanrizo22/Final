b3c9542cb9e1fc9dff29692a49e6503ddbd498ec
rubenexpensive <107783098+rubenexpensive@users.noreply.github.com>
2023-11-26 22:10:32 -0500

    Create main_csv.py

---
+++ main_csv.py
@@ -0,0 +1,17 @@
+from modelo_csv import CSVModel
+from vista_csv import VentanaCSV
+import sys
+from PyQt5.QtWidgets import QApplication
+from controller_csv import Coordinador
+
+def main():
+    app = QApplication(sys.argv)
+    mi_modelo = CSVModel()
+    mi_vista = VentanaCSV()
+    mi_coordinador = Coordinador(mi_vista, mi_modelo)
+    mi_vista.setCoordinador(mi_coordinador)
+    mi_vista.show()
+    sys.exit(app.exec_())
+
+if __name__ == "__main__":
+    main()
