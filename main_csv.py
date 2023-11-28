1ff1ab0aa1a3e5579d4c97314e6dc19076a2b948
rubenexpensive <107783098+rubenexpensive@users.noreply.github.com>
2023-11-27 20:19:08 -0500

    Update main_csv.py

--- main_csv.py
+++ main_csv.py
@@ -1,21 +1,18 @@
 from modelo_csv import CSVModel
 from vista_csv import VentanaCSV
 import sys
-from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5.QtWidgets import QApplication
 from controller_csv import Coordinador
 
 def mainCSV():
+    app = QApplication(sys.argv)
     mi_modelo = CSVModel()
     mi_vista = VentanaCSV()
     mi_coordinador = Coordinador(mi_vista, mi_modelo)
     mi_vista.setCoordinador(mi_coordinador)
-    mi_vista.setup()
-    return mi_vista
+    return mi_vista  # Devuelve la instancia de VentanaCSV
 
 if __name__ == "__main__":
-    app = QApplication(sys.argv)
-    main_window = QMainWindow()
-    main_mat_window = mainCSV()
-    main_window.setCentralWidget(main_mat_window)
-    main_window.show()
-    sys.exit(app.exec_())
+    csv_window = mainCSV()
+    csv_window.show()
+    sys.exit(csv_window.app.exec_())
